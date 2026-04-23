e53b77b8a79b381c6a5f5e58830a33c7479fbee4
kikepb7 <enriquepalmadev@gmail.com>
2026-04-24 01:36:56 +0200

    MATCH | Update gradle to implement match feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
 	implementation(projects.chat)
 	implementation(projects.notification)
 	implementation(projects.club)
+	implementation(projects.match)
 	implementation(projects.common)
 
 	implementation(libs.kotlin.reflect)
---
+++ match/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("java-library")
+    id("squadfy.spring-boot-service")
+    kotlin("plugin.jpa")
+}
+
+group = "org.example"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/milestone") }
+    maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+    implementation(projects.common)
+
+    implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.postgresql)
+
+    implementation(libs.spring.boot.starter.validation)
+    implementation(libs.spring.boot.starter.amqp)
+    implementation(libs.jackson.datatype)
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
